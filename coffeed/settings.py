6833c969c86c5b7b1cbc136c2c172d8742e04854
Berg <BergOSx@Bergs-Mac-Pro.local>
2015-04-17 06:48:19 -0700

    once more

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -13,11 +13,15 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#MAIN_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  didn't work
+#MAIN_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  didn't work
+
+###BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+##MAIN_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))  gave collectstatus configuration error
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MAIN_DIR = os.path.dirname(os.path.dirname(__file__)))
+MAIN_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
